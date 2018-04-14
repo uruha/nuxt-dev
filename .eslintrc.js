bf38d5631a17624de58e2f968c6a886e0636369c
uruha <kochmusica@gmail.com>
2018-04-15 05:24:32 +0900

    add linter prettier

--- .eslintrc.js
+++ .eslintrc.js
@@ -11,11 +11,10 @@ module.exports = {
     // https://github.com/vuejs/eslint-plugin-vue#priority-a-essential-error-prevention
     // consider switching to `plugin:vue/strongly-recommended` or `plugin:vue/recommended` for stricter rules.
     'plugin:vue/essential'
+    // 'plugin:prettier/recommended'
   ],
   // required to lint *.vue files
-  plugins: [
-    'vue'
-  ],
+  plugins: ['vue', 'prettier'],
   // add your custom rules here
   rules: {}
-}
+};
