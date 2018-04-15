0f1f25a1134f258cdf5d479576489a58815a384f
uruha <kochmusica@gmail.com>
2018-04-15 23:00:56 +0900

    add root pseudo styles

--- pages/styles.vue
+++ pages/styles.vue
@@ -1,14 +1,22 @@
 <template>
   <div>
-      <div class="pseudo" data-pseudo-width="100">
-          not support ...
+      <div class="pseudo" data-pseudo-color="" data-pseudo-width="100" data-pseudo-content="Yes support content :)">
+          not support attr for width ...
       </div>
   </div>
 </template>
 
 <style>
+  :root {
+      --red-color: attr(data-pseudo-color); /** not support ... */
+      --red-color: red;
+  }
   .pseudo::before {
       background-color: #000;
-      width: attr(data-pseudo-width px)
+      width: attr(data-pseudo-width px);
+  }
+  .pseudo::after {
+      color: var(--red-color);
+      content: attr(data-pseudo-content);
   }
 </style>
