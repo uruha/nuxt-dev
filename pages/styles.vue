7a8817d4066aeeeabe78eebc0ab11a727e273ce3
uruha <kochmusica@gmail.com>
2018-04-15 22:17:15 +0900

    add css next style sandbox page

---
+++ pages/styles.vue
@@ -0,0 +1,14 @@
+<template>
+  <div>
+      <div class="pseudo" data-pseudo-width="100">
+          not support ...
+      </div>
+  </div>
+</template>
+
+<style>
+  .pseudo::before {
+      background-color: #000;
+      width: attr(data-pseudo-width px)
+  }
+</style>
